6d7329ef2aa6af7284a6e3f1639d7d37cf8aaa87
Fedor Konovalov <fedykonovalov@gmail.com>
2022-04-16 09:13:03 +0300

    Change ROPSTEN_URL to RINKEBY_URL Add Binance Smart Chain(BSC) Testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,8 +26,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    bsct: {
+      url: process.env.BSCT_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
